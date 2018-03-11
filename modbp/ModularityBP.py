fixed retrieval mod calculation

modbp/ModularityBP.py
@@ -1,7 +1,10 @@
 import numpy as np
 import igraph as ig
+from future.utils import iteritems,iterkeys
+from collections import Hashable
 import sklearn.metrics as skm
 from .bp import BP_Modularity,PairVector
+import itertools
 
 class RandomGraph():
     def __init__(self):
@@ -88,7 +91,7 @@ class ModularityBP():
         self.marginals={} # should we keep these?
         self.partitions={} # max of marginals
         self.niters={}
-        self.degrees=self.graph.degree()
+        self.degrees=np.array(self.graph.degree())
         self.edgelist = self._get_edgelist()
         self._edgelistpv= self._get_edgelistpv()
         self._bpmod=None
@@ -96,10 +99,12 @@ class ModularityBP():
     def run_modbp(self,beta,q,niter=100):
         if self._bpmod is None:
             self._bpmod=BP_Modularity(self._edgelistpv,_n=self.n,q=q,beta=beta)
+            self._bpmod.run(niter)
         else:
             self._bpmod.setBeta(beta)
             self._bpmod.setq(q)
             self._bpmod.run(niter)
+
         cmargs=np.array(self._bpmod.return_marginals())
         cpartition = np.argmax(cmargs, axis=1)
         #assure it is initialized
@@ -126,10 +131,9 @@ class ModularityBP():
         _edgelistpv = PairVector(self.edgelist) #cpp wrapper for list
         return _edgelistpv
 
-
     def _get_retrival_modularity(self,beta,q):
-        '''calculate retrival modularity from retrival modularity'''
-        #check for presence and run if not available.
+        '''
+        '''
 
         try:
             cpartition=self.partitions[q][beta]
@@ -137,17 +141,49 @@ class ModularityBP():
             self.run_modbp(beta,q)
             cpartition=self.partitions[q][beta]
 
+        #we sort indices into alike
+        com_inddict = {}
+        allcoms = sorted(list(set(cpartition)))
+        sumA = 0
 
-        def _mod (a):
-            i=a[0]
-            j=a[1]
-            if cpartition[i]==cpartition[j]:
-                #TODO make this weighted and add gamma
-                return (1.0-(self.degrees[i]*self.degrees[j])/(2.0*self.m))
+        # store indices for each community together in dict
+        for i, val in enumerate(cpartition):
+            try:
+                com_inddict[val] = com_inddict.get(val, []) + [i]
+            except TypeError:
+                raise TypeError("Community labels must be hashable- isinstance(%s,Hashable): " % (str(val)), \
+                                isinstance(val, Hashable))
+
+        #first part is sum over edges so calculate from edge list
+        def _mod(a):
+            i = a[0]
+            j = a[1]
+            if cpartition[i] == cpartition[j]:
+                # TODO make this weighted and add gamma
+                return 1.0
             else:
                 return 0.0
-        t=np.apply_along_axis(func1d=_mod,arr=self.edgelist,axis=1)
+        Ahat=np.sum(np.apply_along_axis(func1d=_mod, arr=self.edgelist, axis=1))
 
-        return 1.0/(self.m)*np.sum(np.apply_along_axis(func1d=_mod,arr=self.edgelist,axis=1))
+        # convert indices to np_array
+        for k, val in iteritems(com_inddict):
+            com_inddict[k] = np.array(val)
+        Phat=0
+        for com in allcoms:
+            cind = com_inddict[com]
+            cdeg=self.degrees[cind]
+            if cind.shape[0]==1:
+                Phat+=cdeg
+            else:
+                cPmat=np.outer(cdeg,cdeg.T)
+                #only sum lower triangular element of each
+                Phat+=np.sum(cPmat[np.tril_indices_from(cPmat,-1)])
+            # if cind.shape[0] == 1:  # throws type error if try to index with scalar
+            #     sumA += np.sum(adj_matrix[cind, cind])
+            # else:
+            #     sumA += np.sum(adj_matrix[np.ix_(cind, cind)])
+        return (1.0/self.m)*(Ahat-(1.0/(2*self.m))*Phat)
+
+
 
 

test/test_modbp.py
@@ -101,14 +101,14 @@ def test_qstar():
 
 def test_modinterface_class():
     n=200
-    q=2
+    q=3
     pin = 5 * q / n
     pout = 0.5 * q / n
     prob_mat = np.identity(q) * pin + (np.ones((q, q)) - np.identity(q)) * pout
     randSBM=modbp.RandomSBMGraph(n,prob_mat)
     mbpinterface=modbp.ModularityBP(randSBM.graph)
-    mbpinterface.run_modbp(beta=1.0,q=2)
-    mbpinterface.run_modbp(beta=.5,q=2)
+    mbpinterface.run_modbp(beta=1.0,q=q)
+    # mbpinterface.run_modbp(beta=.5,q=2)
     print (mbpinterface.retrival_modularities)
 
 def main():