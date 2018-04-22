2c017691ce207d1e238f3c249f24a383e33fcbfa
bwalker1 <bwalker@unc.edu>
2018-04-22 03:13:10 -0400

    more typos

--- test/runmanygraphs.sh
+++ test/runmanygraphs.sh
@@ -3,6 +3,6 @@ for eta in $(seq 0.0 0.05 1.01)
 do
     for eps in $(seq 0.0 0.05 1.01)
     do
-        sbatch -t 60 -n 1 -o test.out -p general --wrap="python rungraph.py 1000 2 10 ${eta} 5 ${eps} 100 1""
+        sbatch -t 60 -n 1 -o test.out -p general --wrap="python rungraph.py 1000 2 10 ${eta} 5 ${eps} 100 1"
     done
 done
