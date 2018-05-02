74d80cc950edbda5a44bd14cb759b7c6475912de
William Weir <wweir@longleaf-login1.its.unc.edu>
2018-05-02 12:40:23 -0400

    modified run_LF_test

--- test/run_LFR_test_with_sbmbp.py
+++ test/run_LFR_test_with_sbmbp.py
@@ -13,7 +13,6 @@ clusterdir="/nas/longleaf/home/wweir/ModBP_proj/ModularityBP_Cpp/"
 
 def create_lfr_graph(n=1000, ep=.1, c=3, mk=10, use_gcc=True):
     benchmarkfile = os.path.join(clusterdir,'binary_networks/benchmark')
-
     parameters = [
         benchmarkfile,
         "-N", '{:d}'.format(n),
