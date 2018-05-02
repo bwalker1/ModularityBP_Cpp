b5becd4f8bd63302a0ad18e17469f4dc1eeecf5c
wweir827 <wweir827@gmail.com>
2018-05-02 12:39:45 -0400

    fixed LFR to have output prefix parameter

--- test/run_LFR_test_with_sbmbp.py
+++ test/run_LFR_test_with_sbmbp.py
@@ -12,7 +12,7 @@ import os
 clusterdir="/nas/longleaf/home/wweir/ModBP_proj/ModularityBP_Cpp/binary_networks"
 
 def create_lfr_graph(n=1000, ep=.1, c=3, mk=10, use_gcc=True):
-    benchmarkfile = os.path.join(clusterdir,'/binary_networks/benchmark')
+    benchmarkfile = os.path.join(clusterdir,'binary_networks/benchmark')
     rprefix=np.random.randint(100000)
     parameters = [
         benchmarkfile,
