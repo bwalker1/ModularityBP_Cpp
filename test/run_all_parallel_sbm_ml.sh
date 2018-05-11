5afea78316794ef26d18225b2548dc02d5231e73
wweir827 <wweir827@gmail.com>
2018-05-11 13:00:40 -0400

    script to call all

---
+++ test/run_all_parallel_sbm_ml.sh
@@ -0,0 +1,14 @@
+for gamma in 0.5 1 1.5
+do
+    for omega in 0 1 2
+    do
+        for eta in $(seq 0.0 0.05 1.01)
+        do
+            for eps in $(seq 0.0 0.05 1.01)
+            do
+                #echo "python rungraph.py 512 2 40 ${eta} 16 ${eps} 100 ${omega} ${gamma}"
+                sbatch -t 500 -n 1 -o test.out -p general --wrap="python run_sbm_ml_test.py 512 2 40 ${eta} 16 ${eps} 100 ${omega} ${gamma}"
+            done
+        done
+    done
+done
