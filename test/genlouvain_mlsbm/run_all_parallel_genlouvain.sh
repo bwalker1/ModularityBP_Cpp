be69d442b8e19dfdaca50671ad8330c590269641
wweir827 <wweir827@gmail.com>
2018-07-19 14:26:48 -0400

    output prints

--- test/genlouvain_mlsbm/run_all_parallel_genlouvain.sh
+++ test/genlouvain_mlsbm/run_all_parallel_genlouvain.sh
@@ -2,9 +2,9 @@ for gamma in 0.5 #1 1.5
 do
     for omega in 0 #1 2
     do
-        for eta in $(seq 0.0 0.4 1.00)
+        for eta in $(seq 0.001 0.4 1.00)
         do
-            for eps in $(seq 0.0 0.4 1.00)
+            for eps in $(seq 0.001 0.4 1.00)
             do
                 #echo "python rungraph.py 512 2 40 ${eta} 16 ${eps} 100 ${omega} ${gamma}"
                 sbatch -t 2 -n 1 -o run_louvain_parallel.out -p general --wrap="python run_genlouvain_mlsbm.py 250 2 20 ${eta} 10 ${eps} 3 ${omega} ${gamma}"
