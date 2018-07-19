188a5ad478c43f6f12f8d14e54d33fd31ddb6cef
wweir827 <wweir827@gmail.com>
2018-07-19 10:29:37 -0400

    fixed paths for cluster version call genlouvain

--- test/genlouvain_mlsbm/run_genlouvain_mlsbm.py
+++ test/genlouvain_mlsbm/run_genlouvain_mlsbm.py
@@ -15,12 +15,13 @@ import scipy.sparse as scispa
 import scipy.io as scio
 
 
-# clusterdir = "/nas/longleaf/home/wweir/ModBP_proj/ModularityBP_Cpp/"
+clusterdir = "/nas/longleaf/home/wweir/ModBP_proj/ModularityBP_Cpp/"
 #clusterdir = "/nas02/home/w/w/wweir/ModBP_proj/ModularityBP_Cpp/"
-clusterdir="/Users/whweir/Documents/UNC_SOM_docs/Mucha_Lab/Mucha_Python/ModBP_gh/ModularityBP_Cpp/" #for testing locally
+#clusterdir="/Users/whweir/Documents/UNC_SOM_docs/Mucha_Lab/Mucha_Python/ModBP_gh/ModularityBP_Cpp/" #for testing locally
 
-matlaboutdir = "/Users/whweir/Documents/UNC_SOM_docs/Mucha_Lab/Mucha_Python/ModBP_gh/ModularityBP_Cpp/test/genlouvain_mlsbm/matlab_transfer_file"
-call_matlabfile="/Users/whweir/Documents/UNC_SOM_docs/Mucha_Lab/Mucha_Python/ModBP_gh/ModularityBP_Cpp/test/genlouvain_mlsbm/call_matlab_genlouvain.sh"
+matlaboutdir = os.path.join(clusterdir,"test/genlouvain_mlsbm/matlab_transfer_file"")
+call_matlabfile = os.path.join(clusterdir,"test/genlouvain_mlsbm/call_matlab_genlouvain.sh")
+
 
 # python run_sbm_ml_test.py 100 2 10 .1 5 .1 1 0.5 1.0
 # python run_sbm_ml_test.py 250 2 20 0 10 0.2 1 2.0 .5
