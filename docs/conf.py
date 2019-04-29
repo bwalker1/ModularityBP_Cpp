wrong types for strength measures were throwing off calculation of marginals and theta

docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# modBP documentation build configuration file, created by
+# CHAMP documentation build configuration file, created by
 # sphinx-quickstart on Tue Jul 11 15:50:43 2017.
 #
 # This file is execfile()d with the current directory set to its
@@ -20,6 +20,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../modbp'))
+sys.path.insert(0, os.path.abspath('./sub_docs'))
 
 
 
@@ -54,26 +55,39 @@ if on_rtd:
     # the below only works for Python3.3+
     # from unittest.mock import MagicMock
     # use this for Python<3.3
-    from mock import Mock as MagicMock
+#    from mock import Mock as MagicMock
+
+#    class Mock(MagicMock):
+#        @classmethod
+#        def __getattr__(cls, name):
+#            return Mock()
+    from unittest.mock import MagicMock
 
     class Mock(MagicMock):
         @classmethod
         def __getattr__(cls, name):
-            return Mock()
-
+            return MagicMock()
     # include the names of your minimal required packages here
-    MOCK_MODULES = ['numpy', 'numpy.random', 'igraph', 'matplotlib.pyplot',
-                    'sklearn.metrics','pandas',
-                    'sklearn']
+    MOCK_MODULES = ['numpy', 'numpy.random', 'igraph', 'louvain', 'matplotlib', 'matplotlib.pyplot',
+                    'matplotlib.cm', 'matplotlib.lines', 'matplotlib.patheffects',
+                    'matplotlib.colors','seaborn','pandas',
+                    'matplotlib.patches',
+                    'matplotlib.path',
+                    'sklearn.metrics',
+                    'sklearn', 'h5py',
+                    'scipy','scipy.sparse','ipython','Cython']
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 else:
     html_theme = 'sphinx_rtd_theme'
 
+    sys.path.insert(0,'/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages' )
 #Have to specify all modules
 #These are for local mocks.
-autodoc_mock_imports=['numpy', 'numpy.random', 'igraph', 'matplotlib.pyplot',
-                    'sklearn.metrics','pandas',
-                    'sklearn']
+autodoc_mock_imports=['numpy','igraph','louvain', 'matplotlib','h5py','Cython',
+                      'matplotlib.cm','matplotlib.colors','matplotlib.patches','seaborn','pandas',
+                      'matplotlib.path','matplotlib.lines','matplotlib.pyplot','matplotlib.patheffects',
+                      'sklearn.metrics','numpy.random','sklearn','ipython',
+                      'scipy','scipy.sparse','scipy.optimize','scipy.stats']
 
 #
 # import mock
@@ -99,9 +113,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'modBP'
-copyright = u'2018, William Weir and Benjamin Walker'
-author = u'William Weir and Benjamin Walker'
+project = u'MultiModbp'
+copyright = u'2019, William Weir'
+author = u'William Weir'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -185,7 +199,7 @@ todo_include_todos = True
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
 #
-# html_title = u'modBP v1'
+# html_title = u'CHAMP v1'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #
@@ -206,7 +220,7 @@ todo_include_todos = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
-# html_static_path = ['docs/_static']
+# html_static_path = ['docs/sub_docs']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -286,7 +300,7 @@ todo_include_todos = True
 # html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'modBPdoc'
+htmlhelp_basename = 'MultiModBPdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -312,7 +326,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'modBP.tex', u'modBP Documentation',
+    (master_doc, 'MultiModBP.tex', u'MultiModBP Documentation',
      u'William Weir', 'manual'),
 ]
 
@@ -338,7 +352,7 @@ latex_documents = [
 #
 # latex_appendices = []
 
-# It false, will not define \strong, \code, 	itleref, \crossref ... but only
+# It false, will not define \strong, \code,     itleref, \crossref ... but only
 # \sphinxstrong, ..., \sphinxtitleref, ... To help avoid clash with user added
 # packages.
 #
@@ -354,7 +368,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'modBP', u'modBP Documentation',
+    (master_doc, 'mulitmodbp', u'MultiModBP Documentation',
      [author], 1)
 ]
 
@@ -369,8 +383,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'modBP', u'modBP Documentation',
-     author, 'modBP', 'One line description of project.',
+    (master_doc, 'MultiModBP', u'MultiModBP Documentation',
+     author, 'MultiModBP', 'A multilayer modularity belief propagation tool for community detection in networks.',
      'Miscellaneous'),
 ]
 

modbp/GenerateGraphs.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import seaborn as sbn
 
 
+"""Set of wrapper classes for easier access to graph information used by multimodbp"""
+
 class RandomGraph(object):
     """
     Wrapper class for igraph with different accessor methods.
@@ -36,23 +38,8 @@ class RandomERGraph(RandomGraph):
 
     def __init__(self,n,p):
         self.graph=ig.Graph.Erdos_Renyi(n=n,p=p,directed=False,loops=False)
+        super(RandomERGraph,self).__init__()
 
-# class RandomERGraph():
-#
-#     def __init__(self,n,p,node_inds=None):
-#         """We pick the number of edges as binomial"""
-#
-#         num_edges=np.random.binomial(n*(n-1),p)
-#         edgelist=set([])
-#         if node_inds is None:
-#             node_inds=np.arange(n)
-#         self.m=num_edges
-#         self.n=n
-#         self.edgelist=np.random.choice(node_inds ,replace=True,size=(2,num_edges))
-#             # edge= (i,j) if i<j else (j,i)
-#             # edgelist.add(edge)
-#
-#         self.edgelist.sort
 
 
 class RandomSBMGraph(RandomGraph):
@@ -88,7 +75,7 @@ class RandomSBMGraph(RandomGraph):
         self.comm_prob_mat=comm_prob_mat
 
 
-            #nodes are assigned on bases of block
+        #nodes are assigned on bases of block
         block=[]
         for i,blk in enumerate(self.block_sizes):
             block+=[i for _ in range(blk)]
@@ -109,7 +96,7 @@ class RandomSBMGraph(RandomGraph):
         Compare partition (labels) of the nodes with the ground truth of the SBM
 
         :param labels: a labeling of the nodes
-        :type labels:
+        :type labels: iterable
         :return: AMI
         :rtype: float
         """
@@ -146,6 +133,10 @@ class RandomSBMGraph(RandomGraph):
         return cnts
 
     def get_observed_cin_cout(self):
+        """
+
+        :return: degree for each of the blocks
+        """
         coms,cnts=np.unique(self.graph.vs['block'],return_counts=True)
 
         ncoms=len(coms)
@@ -154,6 +145,7 @@ class RandomSBMGraph(RandomGraph):
                      np.outer(cnts,cnts)-np.diag(cnts)) #N/(N_A*N_B)
         label2num=dict(zip(coms,range(ncoms)))
         observed_cnts=np.zeros((ncoms,ncoms))
+
         for ei, ej in self.get_edgelist():
             ind1=label2num[self.graph.vs['block'][ei]]
             ind2=label2num[self.graph.vs['block'][ej]]
@@ -165,8 +157,8 @@ class RandomSBMGraph(RandomGraph):
 
     def get_accuracy(self, labels):
         """
-        :param labels:
-        :type labels:
+        :param labels: labels to compare to known blocks
+        :type labels: list
         :return:
         :rtype:
         """
@@ -542,8 +534,19 @@ class MultilayerSBM(MultilayerGraph):
     Subclass of MultilayerGraph to create the dynamic stochastic block model from Ghasemian et al. 2016.
     """
     def __init__(self,n,comm_prob_mat,layers=2,transition_prob=.1,block_sizes0=None,use_gcc=False):
+        """
 
-        self.layer_sbms = []
+        :param n: number of nodes in each layer
+        :param comm_prob_mat: probability of block connections in SBM(this is fixed across all layers)
+        :param layers:  number of layers
+        :param transition_prob: probability of each node changing communities from one layer to next.  \
+        if transistion_prob=0 then community structure is constant across all layers.
+        :param block_sizes0: Initial size of the blocks.  Default is to use even block sizes starting out.\
+        block sizes at subsequent layers are determined by number of nodes that randomly transition between \
+        communities
+        :param use_gcc: use only giant connected component of starting SBM (default is False)
+        """
+        self.layer_sbms = []  #this is list of GenerateGraph.RandomSBMGraph objects.
         self.nlayers=layers
         self.transition_prob=transition_prob
         self.comm_prob_mat=comm_prob_mat
@@ -569,7 +572,7 @@ class MultilayerSBM(MultilayerGraph):
 
         for _ in range(layers-1):
             #create the next sbm from the previous one and add it to the list.
-            self.layer_sbms.append(self.get_next_sbm(self.layer_sbms[-1]))
+            self.layer_sbms.append(self._get_next_sbm(self.layer_sbms[-1]))
         self.interedges=self.get_interlayer_edgelist()
         self.intraedges=self.get_intralayer_edgelist()
         self.layer_vec=self.get_node_layer_vec()
@@ -582,11 +585,11 @@ class MultilayerSBM(MultilayerGraph):
         self.layers=[ lsbm.graph for i,lsbm in enumerate(self.layer_sbms)]
         # self.layers=self.layer_sbms #we only need to store these once.
 
-    def get_next_sbm(self,sbm):
+    def _get_next_sbm(self, sbm):
         """
         Generate new block values for each nodes. And then create a new SBM for the next layer
-        :param sbm:
-        :return:
+        :param sbm: current sbm
+        :return: GenerateGraph.RandomSBMGraph
         """
 
         num_transition=np.random.binomial(self.n,self.transition_prob)
@@ -619,6 +622,10 @@ class MultilayerSBM(MultilayerGraph):
         return nxtsbm
 
     def get_intralayer_adj(self):
+        """
+        Single adjacency matrix representing all layers.
+        :return: np.array of intralayer adjacency representation
+        """
         intra_adj=np.zeros((self.n*self.nlayers,self.n*self.nlayers))
         for i,layer in enumerate(self.layer_sbms):
             offset=self.n*i
@@ -627,6 +634,11 @@ class MultilayerSBM(MultilayerGraph):
         return intra_adj
 
     def get_interlayer_adj(self):
+        """
+        Singer interlayer adjencency matrix created by connecting each node to it's equivalent\
+        node in the next layer.  This is a temporal multiplex topology.
+        :return: np.array of interlayer adjacency representation
+        """
         #TODO this can be changed for different couplings. Right now I do only adjacent layers
         inter_adj=np.zeros((self.n*self.nlayers,self.n*self.nlayers))
         Iden_N=np.identity(self.n)
@@ -644,7 +656,7 @@ class MultilayerSBM(MultilayerGraph):
         Single list of edges giving the multilayer connections.  For this model \
         nodes are multiplex an connected to their neighboring slice identities.
 
-        :return:
+        :return: np.array
         """
 
         interedges=np.zeros((self.n*(self.nlayers-1),2),dtype='int')
@@ -660,7 +672,10 @@ class MultilayerSBM(MultilayerGraph):
         return interedges
 
     def get_node_layer_vec(self):
+        """
 
+        :return: np.array denoting which layer each node is in
+        """
         layers=[]
         for i,net in enumerate(self.layer_sbms):
             layers.extend([i for _ in range(net.n)])
@@ -669,9 +684,9 @@ class MultilayerSBM(MultilayerGraph):
     def get_intralayer_edgelist(self):
         """
         Single list of edges treating the group of single layer SBM's  as a \
-        surpaadjacency format
+        surpra-adjacency format
 
-        :return:
+        :return: np.array
         """
         nedges=np.sum([net.m for net in self.layer_sbms])
         intraedges=np.zeros((nedges,2),dtype='int')