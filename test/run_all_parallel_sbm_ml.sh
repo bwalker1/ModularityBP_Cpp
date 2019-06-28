dee291b477a56ae5925383b16f3a4725ac8795a0
William Weir <wweir@longleaf-login1.its.unc.edu>
2019-06-28 16:21:55 -0400

    change of files to run on cluster

--- test/run_all_parallel_sbm_ml.sh
+++ test/run_all_parallel_sbm_ml.sh
@@ -1,13 +1,13 @@
-for gamma in 1 1.5
+for gamma in 1 1.5 3.0
 do
     for omega in 0
     do
         for eta in $(seq 0.0 0.02 1.00)
         do
-            for eps in $(seq 0.0 0.02 .2)
+            for eps in $(seq 0.0 0.02 .42)
             do
                 #echo "python rungraph.py 512 2 40 ${eta} 16 ${eps} 100 ${omega} ${gamma}"
-                sbatch -t 600 -n 1 -o run_sbm_many.out -p general --wrap="python run_sbm_ml_test.py 250 2 20 ${eta} 10 ${eps} 50 ${omega} ${gamma}"
+                sbatch -t 1600 -n 1 -o run_sbm_many.out -p general --wrap="python run_sbm_ml_test.py 250 2 20 ${eta} 10 ${eps} 51 ${omega} ${gamma}"
             done
         done
     done
