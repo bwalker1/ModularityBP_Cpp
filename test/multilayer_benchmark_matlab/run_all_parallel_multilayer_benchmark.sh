1195e4eef4745a1e32f148640b4260d1eede2fb6
William Weir <wweir@longleaf-login1.its.unc.edu>
2019-11-14 08:21:31 -0500

    some changes on cluster to run scritp

--- test/multilayer_benchmark_matlab/run_all_parallel_multilayer_benchmark.sh
+++ test/multilayer_benchmark_matlab/run_all_parallel_multilayer_benchmark.sh
@@ -1,3 +1,4 @@
+#! /usr/bin/bash
 gammas=( 1.0 )
 omegas=( 0  1.000e-03, 3.72e-03, 1.389e-02, 5.179e-02, 1.931e-01, 7.197e-01, 2.682e+00, 1.000e+01 )
 #gammas=(1.0)
@@ -5,7 +6,7 @@ omegas=(1.0)
 #ps=( .5 .85 .95 .99 1.0 )
 ps=( .99 )
 #mus=( seq 0 1 .1 )
-mus=(.1 .2
+mus=( .1 .2 )
 for omega in "${omegas[@]}"
     do
     for gamma in "${gammas[@]}"
@@ -15,7 +16,7 @@ for omega in "${omegas[@]}"
             for mu in "${mus[@]}"
                 do
 	            #echo "${eps} ${gamma} ${omega}"
-                sbatch -t 1000 -n 1 -o /nas/longleaf/home/wweir/ModBP_proj/ModularityBP_Cpp/test/multilayer_benchmark_matlab/test_mulitlayer_runs.txt -p general \
+                sbatch -t 100 -n 1 -o /nas/longleaf/home/wweir/ModBP_proj/ModularityBP_Cpp/test/multilayer_benchmark_matlab/test_mulitlayer_runs.txt -p general \
                 --wrap "python3 /nas/longleaf/home/wweir/ModBP_proj/ModularityBP_Cpp/test/multilayer_benchmark_matlab/run_multilayer_matlab_test.py\
                 1000 15 ${mu} ${p} ${omega} ${gamma} 100"
                 done
