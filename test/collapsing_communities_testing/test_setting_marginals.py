101aead4ea5691df08b9cd48fbae484eb8786d7c
wweir827 <dubyahw827@gmail.com>
2019-12-07 14:47:34 -0500

    testing setting the marginals and running

--- test/collapsing_communities_testing/test_setting_marginals.py
+++ test/collapsing_communities_testing/test_setting_marginals.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sklearn.metrics as skm
 from time import time
+import gzip,pickle
 import os,sys
 sys.path.append(os.path.abspath("../multilayer_benchmark_matlab"))
 from create_multiplex_functions import create_multiplex_graph
 from create_multiplex_functions import create_multiplex_graph_matlab
 
 
-def create_marginals_from_comvec(commvec,q=None,SNR=1000):
+def create_marginals_from_comvec(commvec,q=None,SNR=3):
     if q is None:
         q=len(np.unique(commvec))
 
@@ -32,7 +33,7 @@ def test_multiplex():
 
     t = time()
 
-    load = False
+    load = True
     if not load:
         multipex = create_multiplex_graph(n_nodes=n, mu=mu, p=p_eta,
                                        n_layers=nlayers, ncoms=ncoms)
@@ -82,7 +83,7 @@ def test_multiplex():
     a.set_title('After setting beliefs back')
     good_beliefs=np.array(bpobj._bpmod.getBeliefs())
     bpobj._set_beliefs(newbeliefs)
-    bpobj.run_modbp(beta=beta,q=ncoms,niter=10,omega=omega,reset=False)
+    bpobj.run_modbp(beta=beta,q=ncoms,niter=100,omega=omega,reset=False)
     bpobj.plot_communities(ind=1,ax=a)
     cami = skm.adjusted_mutual_info_score(bpobj.partitions[1], bpobj.graph.comm_vec)
     a.text(s='AMI={:.3f}'.format(cami), x=.1, y=.1, transform=a.transAxes)
