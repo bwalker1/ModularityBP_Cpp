acc840659b58c069920e2d5735a42b5844ab3fa1
William Weir <wweir@longleaf-login1.its.unc.edu>
2020-01-04 08:40:30 -0500

    cluster

--- test/DSBM_experiments/run_all_parallel_sbm_ml.sh
+++ test/DSBM_experiments/run_all_parallel_sbm_ml.sh
@@ -7,7 +7,7 @@ do
             for eps in $(seq 0.0 0.02 1.00)
             do
                 #echo "python rungraph.py 512 2 40 ${eta} 16 ${eps} 100 ${omega} ${gamma}"
-                sbatch -t 1600 -n 1 -o run_sbm_many.out -p general --wrap="python run_sbm_ml_test.py 250 2 20 ${eta} 10 ${eps} 50 ${omega} ${gamma}"
+                sbatch -t 1600 -n 1 -o run_sbm_many.out -p general --wrap="python3 run_sbm_ml_test.py 250 2 20 ${eta} 10 ${eps} 50 ${omega} ${gamma}"
             done
         done
     done
