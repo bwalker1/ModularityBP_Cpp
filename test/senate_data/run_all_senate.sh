0466385ba0b59c1e105c278f57b6f2b3e9189468
wweir827 <wweir827@gmail.com>
2020-01-06 13:40:42 -0500

    update run senate

--- test/senate_data/run_all_senate.sh
+++ test/senate_data/run_all_senate.sh
@@ -3,6 +3,6 @@ do
     for omega in `seq 0 .05 12`
     do
         #echo "sbatch -t 800 -n 1 -o senate_run.out -p general --wrap=\"python run_modbp_senate.py ${gamma} ${omega}\""
-        sbatch -t 200 -n 1 -o senate_run.out -p general --wrap="python run_modbp_senate.py ${gamma} ${omega}"
+        sbatch -t 200 -n 1 -o senate_run.out -p general --wrap="python3 run_modbp_senate.py ${gamma} ${omega}"
     done
 done
